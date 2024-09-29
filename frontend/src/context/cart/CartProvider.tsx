16903bc700f37781e8efdfe97b3f1b9af3cb9c87
Khaled_mahran_19 <136863971+kkhaled119@users.noreply.github.com>
2024-09-29 09:38:58 +0300

    delet and update cart

--- frontend/src/context/cart/CartProvider.tsx
+++ frontend/src/context/cart/CartProvider.tsx
@@ -21,10 +21,11 @@ const CartProvider: FC<PropsWithChildren> = ({ children }) => {
         },
       });
       if (!response.ok) {
-        setError("Faild to featch user cart.Please try again");
+        setError("Failed to fetch user cart. Please try again.");
+        return;
       }
       const cart = await response.json();
-      const cartItemsMaaped = cart.items.map(
+      const cartItemsMapped = cart.items.map(
         ({
           product,
           quantity,
@@ -42,14 +43,13 @@ const CartProvider: FC<PropsWithChildren> = ({ children }) => {
           unitPrice,
         })
       );
-      setCartItems(cartItemsMaaped);
+      setCartItems(cartItemsMapped);
       setTotalAmount(cart.totalAmount);
     };
     fetchCart();
   }, [token]);
 
   const addItemToCart = async (productId: string) => {
-    console.log(productId);
     try {
       const response = await fetch(`${BASE_URL}/cart/items`, {
         method: "POST",
@@ -63,30 +63,130 @@ const CartProvider: FC<PropsWithChildren> = ({ children }) => {
         }),
       });
       if (!response.ok) {
-        setError("Faild to add to cart");
+        setError("Failed to add to cart.");
+        return;
       }
       const cart = await response.json();
       if (!cart) {
-        setError("Faild to cart items");
+        setError("Failed to fetch cart items.");
+        return;
       }
-      const cartItemsMaaped = cart.items.map(
+      const cartItemsMapped = cart.items.map(
         ({ product, quantity }: { product: any; quantity: number }) => ({
           productId: product._id,
           title: product.title,
           image: product.image,
           price: product.price,
           quantity,
-          unitePrice: product.unitePrice,
+          unitPrice: product.unitPrice,
         })
       );
-      setCartItems([...cartItemsMaaped]);
+      setCartItems([...cartItemsMapped]);
       setTotalAmount(cart.totalAmount);
     } catch (error) {
       console.log(error);
     }
   };
+
+  const updateItemInCart = async (productId: string, quantity: number) => {
+    try {
+      const response = await fetch(`${BASE_URL}/cart/items`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          productId,
+          quantity,
+        }),
+      });
+      if (!response.ok) {
+        setError("Failed to update cart.");
+        return;
+      }
+      const cart = await response.json();
+      if (!cart) {
+        setError("Failed to parse cart.");
+        return;
+      }
+      const cartItemsMapped = cart.items.map(
+        ({
+          product,
+          quantity,
+          unitPrice,
+        }: {
+          product: any;
+          quantity: number;
+          unitPrice: number;
+        }) => ({
+          productId: product._id,
+          title: product.title,
+          image: product.image,
+          price: product.price,
+          quantity,
+          unitPrice,
+        })
+      );
+      setCartItems([...cartItemsMapped]);
+      setTotalAmount(cart.totalAmount);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeItemInCart = async (productId: string) => {
+    try {
+      const response = await fetch(`${BASE_URL}/cart/items/${productId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        setError("Failed to delete item from cart.");
+        return;
+      }
+      const cart = await response.json();
+      if (!cart) {
+        setError("Failed to parse cart.");
+        return;
+      }
+      const cartItemsMapped = cart.items.map(
+        ({
+          product,
+          quantity,
+          unitPrice,
+        }: {
+          product: any;
+          quantity: number;
+          unitPrice: number;
+        }) => ({
+          productId: product._id,
+          title: product.title,
+          image: product.image,
+          price: product.price,
+          quantity,
+          unitPrice,
+        })
+      );
+      setCartItems([...cartItemsMapped]);
+      setTotalAmount(cart.totalAmount);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <CartContext.Provider value={{ cartItems, totalAmount, addItemToCart }}>
+    <CartContext.Provider
+      value={{
+        cartItems,
+        totalAmount,
+        addItemToCart,
+        updateItemInCart,
+        removeItemInCart,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -3,7 +3,19 @@ import { useCart } from "../context/cart/CartContext";
 import { Button, ButtonGroup } from "@mui/material";
 
 const CartPage = () => {
-  const { cartItems, totalAmount } = useCart();
+  const { cartItems, totalAmount, updateItemInCart, removeItemInCart } =
+    useCart(); // تأكد من استدعاء removeItemInCart من الـ context
+
+  const handleQuantity = (productId: string, quantity: number) => {
+    if (quantity <= 0) {
+      return;
+    }
+    updateItemInCart(productId, quantity);
+  };
+
+  const handleRemoveItem = (productId: string) => {
+    removeItemInCart(productId);
+  };
 
   return (
     <Container fixed sx={{ mt: 2 }}>
@@ -26,22 +38,35 @@ const CartPage = () => {
             <Box display="flex" flexDirection="row" alignItems="center" gap={3}>
               <img src={item.image} width={90} alt={item.title} />
               <Box>
-                <Typography variant="h5"> {item.title}</Typography>
+                <Typography variant="h5">{item.title}</Typography>
                 <Typography>
-                  {item.quantity} x {item.unitePrice || item.price}$
+                  {item.quantity} x {item.unitPrice}$
                 </Typography>
-                <Button>remove</Button>
+                <Button onClick={() => handleRemoveItem(item.productId)}>
+                  Remove
+                </Button>
               </Box>
             </Box>
             <ButtonGroup variant="contained" aria-label="Basic button group">
-              <Button>-</Button>
-              <Button>+</Button>
+              <Button
+                onClick={() =>
+                  handleQuantity(item.productId, item.quantity - 1)
+                }
+              >
+                -
+              </Button>
+              <Button
+                onClick={() =>
+                  handleQuantity(item.productId, item.quantity + 1)
+                }
+              >
+                +
+              </Button>
             </ButtonGroup>
           </Box>
         ))}
         <Typography variant="h4">
-          {" "}
-          <Box>Total Amount:{totalAmount} $</Box>
+          <Box>Total Amount: {totalAmount} $</Box>
         </Typography>
       </Box>
     </Container>
